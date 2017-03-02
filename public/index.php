8e54f76e92a933ab51bfd7518e1113a1a879b445
Robert Cox <rcox@dontbebroke.com>
2017-03-02 09:00:32 -0800

    Seperated MOST of the logic from the view

--- public/index.php
+++ public/index.php
@@ -1,12 +1,95 @@
 <?php // vim: et
 
-require_once __DIR__ . '/../src/autoloader.php';    // Autoload Classes
+require_once __DIR__ . '/../src/autoloader.php';
 
 include_once __DIR__ . '/include/pregen-news.inc';
 include_once __DIR__ . '/include/pregen-confs.inc';
 include_once __DIR__ . '/include/prepend.inc';
 include_once __DIR__ . '/include/branches.inc';
 include_once __DIR__ . '/include/version.inc';
+
+// Get the modification date of this PHP file
+$timestamps = array(@getlastmod());
+
+/*
+   The date of prepend.inc represents the age of ALL
+   included files. Please touch it if you modify any
+   other include file (and the modification affects
+   the display of the index page). The cost of stat'ing
+   them all is prohibitive.
+*/
+$timestamps[] = @filemtime(__DIR__ . "/../../public/include/prepend.inc");
+
+// These are the only dynamic parts of the frontpage
+$timestamps[] = @filemtime(__DIR__ . "/../../public/include/pregen-confs.inc");
+$timestamps[] = @filemtime(__DIR__ . "/../../public/include/pregen-news.inc");
+$timestamps[] = @filemtime(__DIR__ . "/../../public/include/version.inc");
+$timestamps[] = @filemtime(__DIR__ . "/../../public/js/common.js");
+
+// The latest of these modification dates is our real Last-Modified date
+$timestamp = max($timestamps);
+
+// Note that this is not a RFC 822 date (the tz is always GMT)
+$tsstring = gmdate("D, d M Y H:i:s ", $timestamp) . "GMT";
+
+// Check if the client has the same page cached
+if (isset($_SERVER["HTTP_IF_MODIFIED_SINCE"]) &&
+    ($_SERVER["HTTP_IF_MODIFIED_SINCE"] == $tsstring)) {
+    header("HTTP/1.1 304 Not Modified");
+    exit();
+}
+// Inform the user agent what is our last modification date
+else {
+    header("Last-Modified: " . $tsstring);
+}
+
+$_SERVER['BASE_PAGE'] = 'index.php';
+
+mirror_setcookie("LAST_NEWS", $_SERVER["REQUEST_TIME"], 60*60*24*365);
+
+$releasenews = 0;
+$frontpage   = array();
+
+foreach($NEWS_ENTRIES as $entry) {
+    $maybe = false;
+    foreach($entry["category"] as $category) {
+        if ($category["term"] == "releases") {
+            if ($releasenews++ > 5) {
+                continue 2;
+            }
+        }
+        if ($category["term"] == "frontpage") {
+            $maybe = $entry;
+        }
+    }
+    if ($maybe) {
+        $frontpage[] = $maybe;
+    }
+}
+
+// Prepare announcements.
+if (is_array($CONF_TEASER)) {
+    $conftype = array(
+        'conference' => 'Upcoming conferences',
+        'cfp'        => 'Conferences calling for papers',
+    );
+    $announcements = "";
+    foreach($CONF_TEASER as $category => $entries) {
+        if ($entries) {
+            $announcements .= '<div class="panel">';
+            $announcements .= '  <a href="/conferences" class="headline" title="' . $conftype[$category] . '">' . $conftype[$category] .'</a>';
+            $announcements .= '<div class="body"><ul>';
+            foreach (array_slice($entries, 0, 4) as $url => $title) {
+                $title = preg_replace("'([A-Za-z0-9])([\s\:\-\,]*?)call for(.*?)$'i", "$1", $title);
+                $announcements .= "<li><a href='$url' title='$title'>$title</a></li>";
+            }
+            $announcements .= '</ul></div>';
+            $announcements .= '</div>';
+        }
+    }
+} else {
+    $announcements = '';
+}
 
 use Services\Templates\Renderer;
 
@@ -15,5 +98,7 @@ $renderer = new Renderer();
 $renderer->render('homepage.php', array(
     'NEWS_ENTRIES' => $NEWS_ENTRIES,
     'CONF_TEASER'  => $CONF_TEASER,
-    'MYSITE'       => $MYSITE
+    'MYSITE'       => $MYSITE,
+    'frontpage'    => $frontpage,
+    'announcements' => $announcements,
 ));
--- src/Views/homepage.php
+++ src/Views/homepage.php
@@ -1,90 +1,5 @@
 <?php
-
-// Get the modification date of this PHP file
-$timestamps = array(@getlastmod());
-
-/*
-   The date of prepend.inc represents the age of ALL
-   included files. Please touch it if you modify any
-   other include file (and the modification affects
-   the display of the index page). The cost of stat'ing
-   them all is prohibitive.
-*/
-$timestamps[] = @filemtime(__DIR__ . "/../../public/include/prepend.inc");
-
-// These are the only dynamic parts of the frontpage
-$timestamps[] = @filemtime(__DIR__ . "/../../public/include/pregen-confs.inc");
-$timestamps[] = @filemtime(__DIR__ . "/../../public/include/pregen-news.inc");
-$timestamps[] = @filemtime(__DIR__ . "/../../public/include/version.inc");
-$timestamps[] = @filemtime(__DIR__ . "/../../public/js/common.js");
-
-// The latest of these modification dates is our real Last-Modified date
-$timestamp = max($timestamps);
-
-// Note that this is not a RFC 822 date (the tz is always GMT)
-$tsstring = gmdate("D, d M Y H:i:s ", $timestamp) . "GMT";
-
-// Check if the client has the same page cached
-if (isset($_SERVER["HTTP_IF_MODIFIED_SINCE"]) &&
-    ($_SERVER["HTTP_IF_MODIFIED_SINCE"] == $tsstring)) {
-    header("HTTP/1.1 304 Not Modified");
-    exit();
-}
-// Inform the user agent what is our last modification date
-else {
-    header("Last-Modified: " . $tsstring);
-}
-
-$_SERVER['BASE_PAGE'] = 'index.php';
-
-
-mirror_setcookie("LAST_NEWS", $_SERVER["REQUEST_TIME"], 60*60*24*365);
-
-
-$content     = "<div class='home-content'>";
-$releasenews = 0;
-$frontpage   = array();
-
-foreach($NEWS_ENTRIES as $entry) {
-    $maybe = false;
-    foreach($entry["category"] as $category) {
-        if ($category["term"] == "releases") {
-            if ($releasenews++ > 5) {
-                continue 2;
-            }
-        }
-        if ($category["term"] == "frontpage") {
-            $maybe = $entry;
-        }
-    }
-    if ($maybe) {
-        $frontpage[] = $maybe;
-    }
-}
-foreach($frontpage as $entry) {
-    $link = substr($entry["id"], 15); // Strip http://php.net/
-    $id   = parse_url($entry["id"], PHP_URL_FRAGMENT);
-    $date = date_create($entry['updated']);
-    $date_human = date_format($date, 'd M Y');
-    $date_w3c = date_format($date, DATE_W3C);
-    $content .= <<<NEWSENTRY
-<article class="newsentry">
-  <header class="title">
-    <time datetime="$date_w3c">$date_human</time>
-    <h2 class="newstitle">
-      <a href="{$MYSITE}{$link}" id="{$id}">{$entry["title"]}</a>
-    </h2>
-  </header>
-  <div class="newscontent">
-    {$entry["content"]}
-  </div>
-</article>
-NEWSENTRY;
-}
-$content .= '<p class="archive"><a href="/archive/">Older News Entries</a></p>';
-$content .= "</div>";
-
-$intro = <<<EOF
+$intro ='
   <div class="row clearfix">
     <div class="blurb">
       <p>PHP is a popular general-purpose scripting language that is especially suited to web development.</p>
@@ -92,22 +7,32 @@ $intro = <<<EOF
     </div>
     <div class="download">
       <h3>Download</h3>
-EOF;
+      <ul>';
+        foreach (get_active_branches() as $major => $releases) :
+          foreach ((array)$releases as $release) :
+            $version                 = $release['version'];
+            list($major, $minor, $_) = explode('.', $version);
 
-$intro .= "<ul>\n";
-foreach (get_active_branches() as $major => $releases) {
-    foreach ((array)$releases as $release) {
-        $version = $release['version'];
-        list($major, $minor, $_) = explode('.', $version);
-        $intro .= "
-            <li><a class='download-link' href='/downloads.php#v$version'>$version</a><span class='dot'>&middot;</span><a class='notes' href='/ChangeLog-$major.php#$version'>Release Notes</a><span class='dot'>&middot;</span><a class='notes' href='/migration$major$minor'>Upgrading</a></li>\n";
-    }
-}
-$intro .= "</ul>\n";
-$intro .= <<<EOF
+            $intro .= "
+              <li>
+                <a class='download-link' href='/downloads.php#v$version'>
+                  $version
+                </a>
+                <span class='dot'>&middot;</span>
+                <a class='notes' href='/ChangeLog-$major.php#$version'>
+                  Release Notes
+                </a>
+                <span class='dot'>&middot;</span>
+                <a class='notes' href='/migration$major$minor'>
+                  Upgrading
+                </a>
+              </li>";
+          endforeach;
+        endforeach;
+$intro .= ' 
+      </ul>
     </div>
-  </div>
-EOF;
+  </div>';
 
 // Write out common header
 site_header("Hypertext Preprocessor",
@@ -139,54 +64,48 @@ site_header("Hypertext Preprocessor",
         'intro' => $intro
     )
 );
+?>
 
-// Print body of home page.
-echo $content;
-
-// Prepare announcements.
-if (is_array($CONF_TEASER)) {
-    $conftype = array(
-        'conference' => 'Upcoming conferences',
-        'cfp'        => 'Conferences calling for papers',
-    );
-    $announcements = "";
-    foreach($CONF_TEASER as $category => $entries) {
-        if ($entries) {
-            $announcements .= '<div class="panel">';
-            $announcements .= '  <a href="/conferences" class="headline" title="' . $conftype[$category] . '">' . $conftype[$category] .'</a>';
-            $announcements .= '<div class="body"><ul>';
-            foreach (array_slice($entries, 0, 4) as $url => $title) {
-                $title = preg_replace("'([A-Za-z0-9])([\s\:\-\,]*?)call for(.*?)$'i", "$1", $title);
-                $announcements .= "<li><a href='$url' title='$title'>$title</a></li>";
-            }
-            $announcements .= '</ul></div>';
-            $announcements .= '</div>';
-        }
-    }
-} else {
-    $announcements = '';
-}
-
-$SIDEBAR = <<< SIDEBAR_DATA
-
-$announcements
-    <p class='panel'><a href='/cal.php'>User Group Events</a></p>
-    <p class='panel'><a href='/thanks.php'>Special Thanks</a></p>
-    <p class='panel social-media'>
-      <span class='headline'>Social media</span>
-      <div class='body'>
-        <ul>
-          <li>
-            <a href="https://twitter.com/official_php">
-              <i class="icon-twitter"></i>
-              @official_php
-            </a>
-          </li>
-        </ul>
-      </div>
-    </p>
-
-SIDEBAR_DATA;
+<div class="home-content">
+<?php foreach($frontpage as $entry) :
+    $link       = substr($entry["id"], 15); // Strip http://php.net/ TODO: remove magic integer
+    $id         = parse_url($entry["id"], PHP_URL_FRAGMENT);
+    $date       = date_create($entry['updated']);
+    $date_human = date_format($date, 'd M Y');
+    $date_w3c   = date_format($date, DATE_W3C);
+?>
+  <article class="newsentry">
+    <header class="title">
+      <time datetime="<?= $date_w3c ?>"><?= $date_human ?></time>
+      <h2 class="newstitle">
+        <a href="<?= $MYSITE; ?><?= $link; ?>" id="<?= $id ?>"><?= $entry["title"] ?></a>
+      </h2>
+    </header>
+    <div class="newscontent">
+      <?= $entry["content"] ?>
+    </div>
+  </article>
+<?php endforeach; ?>
+  <p class="archive"><a href="/archive/">Older News Entries</a></p>
+</div>
+<?php
+$SIDEBAR = "
+  $announcements
+  <p class='panel'><a href='/cal.php'>User Group Events</a></p>
+  <p class='panel'><a href='/thanks.php'>Special Thanks</a></p>
+  <p class='panel social-media'>
+    <span class='headline'>Social media</span>
+    <div class='body'>
+      <ul>
+        <li>
+          <a href='https://twitter.com/official_php'>
+            <i class='icon-twitter'></i>
+            @official_php
+          </a>
+        </li>
+      </ul>
+    </div>
+  </p>";
 
 // Print the common footer.
 site_footer(
